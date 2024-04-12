87fb9f80c4423e24547731170494b549d3422b9e
Shisha1536 <schischkoff2017@yandex.ru>
2024-04-12 13:10:37 +0300

    dz_3+

---
+++ dz_3/src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero() {
+    return(
+        <React.Fragment>
+        <h1>Добро пожаловать в наш магазин</h1>
+        <p>Начать покупки!</p>
+    </React.Fragment>
+    )
+    
+}
---
+++ dz_3/src/components/Welcome.tsx
@@ -0,0 +1,3 @@
+export default function Welcome(props: string) {
+    <h1>{props}</h1>
+}
